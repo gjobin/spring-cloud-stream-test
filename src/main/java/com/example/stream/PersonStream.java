730da4e5e7da78780a2e24d60a77d60b6e0dfa11
Guillaume Jobin <guillaume.jobin@ticketmaster.com>
2023-09-07 10:15:51 -0400

    Simplify and fix producer error

--- src/main/java/com/example/stream/PersonStream.java
+++ src/main/java/com/example/stream/PersonStream.java
@@ -3,12 +3,8 @@ package com.example.stream;
 import com.example.model.Person;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
-import org.springframework.kafka.support.KafkaHeaders;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 
-import java.util.UUID;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
@@ -18,12 +14,10 @@ import java.util.function.Supplier;
 public class PersonStream {
 
     @Bean
-    public Supplier<Message<Person>> source() {
-        return () -> MessageBuilder.withPayload(Person.builder()
-                        .firstname("fn")
-                        .lastname("ln")
-                        .build())
-                .setHeader(KafkaHeaders.KEY, UUID.randomUUID().toString())
+    public Supplier<Person> source() {
+        return () -> Person.builder()
+                .firstname("fn")
+                .lastname("ln")
                 .build();
     }
 
--- src/test/java/com/example/stream/PersonStreamTest.java
+++ src/test/java/com/example/stream/PersonStreamTest.java
@@ -1,13 +1,10 @@
 package com.example.stream;
 
+import com.example.Application;
 import com.example.model.Person;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.github.valfirst.slf4jtest.TestLogger;
-import com.github.valfirst.slf4jtest.TestLoggerFactory;
-import com.github.valfirst.slf4jtest.TestLoggerFactoryExtension;
-import com.example.Application;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.stream.binder.test.InputDestination;
@@ -20,17 +17,12 @@ import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.support.GenericMessage;
 
 import java.io.IOException;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
+import java.util.Collections;
 
-import static com.github.valfirst.slf4jtest.LoggingEvent.info;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatIterable;
 
 @SpringBootTest(classes = Application.class)
 @Import({TestChannelBinderConfiguration.class})
-@ExtendWith(TestLoggerFactoryExtension.class)
 @EmbeddedKafka
 class PersonStreamTest {
 
@@ -42,40 +34,24 @@ class PersonStreamTest {
     @Autowired
     private OutputDestination output;
 
+    @BeforeEach
+    public void beforeEach(){
+        output.clear();
+    }
 
     @Test
     void producerTest() {
-        Message<Person> message = receiveMessage(Person.class, "thasource.destination");
+        Message<Person> message = receiveMessage(Person.class, "source-out-0");
         assertThat(message.getPayload()).isEqualTo(Person.builder().firstname("fn").lastname("ln").build());
-        String key = (String) message.getHeaders().get("kafka_messageKey");
-        assert key != null;
-        assertThat(UUID.fromString(key).toString()).isEqualTo(key);
     }
 
     @Test
     void functionTest() {
-        MessageHeaders messageHeaders = new MessageHeaders(Map.of("kafka_messageKey", "theKey"));
-        Message<Person> sendMessage = new GenericMessage<>(Person.builder().firstname("firstname").lastname("lastname").build(), messageHeaders);
-        output.clear();
-        input.send(sendMessage, "thasource");
+        Message<Person> sendMessage = new GenericMessage<>(Person.builder().firstname("firstname").lastname("lastname").build(), new MessageHeaders(Collections.emptyMap()));
+        input.send(sendMessage, "source-out-0");
 
-        Message<Person> message = receiveMessage(Person.class, "uppercased");
+        Message<Person> message = receiveMessage(Person.class, "sink-in-0");
         assertThat(message.getPayload()).isEqualTo(Person.builder().firstname("firstname").lastname("LASTNAME").build());
-        String key = (String) message.getHeaders().get("kafka_messageKey");
-        assert key != null;
-        assertThat(key).isEqualTo("theKey");
-    }
-
-    @Test
-    void consumerTest() {
-        MessageHeaders messageHeaders = new MessageHeaders(Map.of("kafka_messageKey", "theKey"));
-        Person entity = Person.builder().firstname("firstname").lastname("LASTNAME").build();
-        Message<Person> sendMessage = new GenericMessage<>(entity, messageHeaders);
-        output.clear();
-
-        TestLogger logger = TestLoggerFactory.getTestLogger(PersonStream.class);
-        input.send(sendMessage, "uppercased");
-        assertThatIterable(logger.getLoggingEvents()).isEqualTo(List.of(info(entity.toString())));
     }
 
     private <T> Message<T> receiveMessage(Class<T> clazz, String bindingName) {
